4f33c83e5ab3e67cf33139e31eb81b101865a08e
WUTBwillis <48959137+WUTBwillis@users.noreply.github.com>
2019-09-20 20:19:22 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_WilliamMurray.py
@@ -0,0 +1,13 @@
+#This program will calculate the sales total to find the annual profit.
+#9/16/19
+#CTI-110 P2T1- Sales Prediction
+#William Murray
+#
+
+total_sales = float(input("Enter the total sales "))
+
+profit = total_sales * 0.23
+
+print("The profit is $", profit)
+
+input()
